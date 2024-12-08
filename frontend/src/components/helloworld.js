c120a0b55ec99b025eaee3f8b7d29fbb4529d8ce
Sahilpreet Singh <147434046+sps1001@users.noreply.github.com>
2024-12-08 12:17:40 +0530

    basic framework added

---
+++ frontend/src/components/helloworld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello Rudra</h1>
+      <h2>Soham baby</h2>
+    </div>
+  );
+};
+
+export default HelloWorld;
